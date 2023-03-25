aa6825b843a9dd4344a9789c803cd8f77e35be7e
mhdryan09 <pranataryan91@gmail.com>
2023-03-25 21:52:07 +0700

    Menambahkan view Login dan Registration

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
-use App\Http\Controllers\PostController;
 use App\Models\Category;
-use App\Models\Post;
-use App\Models\User;
+
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -67,3 +68,8 @@ Route::get('/categories', function () {
         ]
     );
 });
+
+
+Route::get('/login', [LoginController::class, 'index']);
+
+Route::get('/register', [RegisterController::class, 'index']);
